1df5326eeb8bf5a33d9ccd41fada85684e9e879b
Grzegorz <grzegorztarkowski8@wp.pl>
2021-03-14 22:51:29 +0100

    Add click functions to selection images

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PaperRockScissors
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        BitmapImage paperIcon = new BitmapImage(new Uri("Images/paper.png", UriKind.RelativeOrAbsolute));
+        const string pathToPaperImage = "Images/paper.png";
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void paperImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            playerMoveImage.Source = paperIcon;
         }
     }
 }
