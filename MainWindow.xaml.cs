b88abf76023b6952a650f88230bfe47414b6a7ea
Grzegorz <grzegorztarkowski8@wp.pl>
2021-03-14 23:16:25 +0100

    Create game mechanism
    I created an enemy movement functions and simple way to check
    who won. But I think it is not optimaly so I will work on it
    to improve it.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,16 +14,89 @@ namespace PaperRockScissors
     
     public partial class MainWindow : Window
     {
+
         BitmapImage paperIcon = new BitmapImage(new Uri("Images/paper.png", UriKind.RelativeOrAbsolute));
-        const string pathToPaperImage = "Images/paper.png";
+        BitmapImage rockIcon = new BitmapImage(new Uri("Images/rock.png", UriKind.RelativeOrAbsolute));
+        BitmapImage scissorsIcon = new BitmapImage(new Uri("Images/scissors.png", UriKind.RelativeOrAbsolute));
+        BitmapImage invertPaperIcon = new BitmapImage(new Uri("Images/rpaper.png", UriKind.RelativeOrAbsolute));
+        BitmapImage invertRockIcon = new BitmapImage(new Uri("Images/rrock.png", UriKind.RelativeOrAbsolute));
+        BitmapImage invertScissorsIcon = new BitmapImage(new Uri("Images/rscissors.png", UriKind.RelativeOrAbsolute));
+        const int PAPER_ID = 1;
+        const int ROCK_ID = 2;
+        const int SCISSORD_ID = 3;
+        static int playerMove;
+        static int enemyMove;
+
         public MainWindow()
         {
             InitializeComponent();
         }
         private void paperImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
             playerMoveImage.Source = paperIcon;
+            playerMove = PAPER_ID;
+            enemyMove = enemyMovement();
+            if(enemyMove==ROCK_ID)
+            {
+                MessageBox.Show("You won");
+            }
+            else if(enemyMove==SCISSORD_ID)
+            {
+                MessageBox.Show("You loose");
+            }
+            else MessageBox.Show("Tie");
+        }
+        private void rockImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            playerMoveImage.Source = rockIcon;
+            playerMove = ROCK_ID;
+            enemyMove = enemyMovement();
+            if (enemyMove == SCISSORD_ID)
+            {
+                MessageBox.Show("You won");
+            }
+            else if (enemyMove == PAPER_ID)
+            {
+                MessageBox.Show("You loose");
+            }
+            else MessageBox.Show("Tie");
+        }
+        private void scissorsImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            playerMoveImage.Source = scissorsIcon;
+            playerMove = SCISSORD_ID;
+            enemyMove = enemyMovement();
+            if (enemyMove == PAPER_ID)
+            {
+                MessageBox.Show("You won");
+            }
+            else if (enemyMove == ROCK_ID)
+            {
+                MessageBox.Show("You loose");
+            }
+            else MessageBox.Show("Tie");
+        }
+
+        private int enemyMovement()
+        {
+            Random rand = new Random();
+            int move = rand.Next(1, 4);
+            if(move==PAPER_ID)
+            {
+                enemyMoveImage.Source = invertPaperIcon;
+                return PAPER_ID;
+            }
+            else if(move==ROCK_ID)
+            {
+                enemyMoveImage.Source = invertRockIcon;
+                return ROCK_ID;
+            }
+            else
+            {
+                enemyMoveImage.Source = invertScissorsIcon;
+                return SCISSORD_ID;
+            }
         }
     }
 }
